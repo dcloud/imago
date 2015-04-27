35e5ec94dc0f2752c7f5f4c8cb5140eb5b66c753
Daniel Cloud <daniel@danielcloud.org>
2015-04-27 11:59:17 -0400

    Being basic tests for bills and jurisdictions.

--- imago/tests/test_jurisdiction_endpoint.py
+++ imago/tests/test_jurisdiction_endpoint.py
@@ -3,26 +3,28 @@ from imago.tests.helpers import ApiRequestFactory, decode_json_or_none
 import pytest
 import django; django.setup()
 
-from imago.views import BillList
+from imago.views import JurisdictionList
 
 
-class BillSearchTests(TestCase):
+class TestJuridictionResource(TestCase):
+
+    """Tests on JurisdictionResource api endpoint"""
 
     def setUp(self):
         self.factory = ApiRequestFactory()
 
     @pytest.mark.django_db
-    def test_bills_http200(self):
-        endpoint = BillList()
-        request = self.factory.get('/bills/')
+    def test_jurisdictions_http200(self):
+        endpoint = JurisdictionList()
+        request = self.factory.get('/jursidictions/')
         response = endpoint.get(request)
         self.assertEqual(response.status_code, 200,
                          msg="Endpoint should return a 200 response when no URL params provided")
 
     @pytest.mark.django_db
-    def test_bills_deserialize(self):
-        endpoint = BillList()
-        request = self.factory.get('/bills/')
+    def test_jurisdictions_deserialize(self):
+        endpoint = JurisdictionList()
+        request = self.factory.get('/jursidictions/')
         response = endpoint.get(request)
         content = decode_json_or_none(response.content)
         if not content:
